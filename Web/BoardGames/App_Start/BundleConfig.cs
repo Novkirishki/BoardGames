30d8110e2f04346a691bb8f060d1c0a62def4bad
Novkirishki <novkirishki94@gmail.com>
2016-02-06 23:05:18 +0200

    Fixed logo image, added jquery count plugin

--- Web/BoardGames/App_Start/BundleConfig.cs
+++ Web/BoardGames/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace BoardGames
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryCountTo").Include(
+                        "~/Scripts/jquery.countTo.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
