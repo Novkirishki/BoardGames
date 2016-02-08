e5d601b09601fa476310c73cb1adfd01eba2343d
Novkirishki <novkirishki94@gmail.com>
2016-02-08 13:46:47 +0200

    Added models

---
+++ Data/BoardGames.Data.Models/Category.cs
@@ -0,0 +1,34 @@
+﻿namespace BoardGames.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Category
+    {
+        private ICollection<Review> reviews;
+
+        public Category()
+        {
+            this.Reviews = new HashSet<Review>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Review> Reviews
+        {
+            get
+            {
+                return this.reviews;
+            }
+
+            set
+            {
+                this.reviews = value;
+            }
+        }
+    }
+}
---
+++ Data/BoardGames.Data.Models/Review.cs
@@ -0,0 +1,45 @@
+﻿namespace BoardGames.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Review
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        public string GameTitle { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Range(1, 50, ErrorMessage = "The minimum players must be between {1} and {2}")]
+        public int MinPlayers { get; set; }
+
+        [Range(2, 50, ErrorMessage = "The maximum players must be between {1} and {2}")]
+        public int MaxPlayers { get; set; }
+
+        [Range(3, 18)]
+        public int MinAgeRequired { get; set; }
+
+        [Range(1, 6000)]
+        public int MinPlayingTimeInMinutes { get; set; }
+
+        [Required]
+        public int CreatorId { get; set; }
+
+        public User Creator { get; set; }
+
+        [MaxLength(2083)]
+        public string UrlToOfficialSite { get; set; }
+    }
+}
