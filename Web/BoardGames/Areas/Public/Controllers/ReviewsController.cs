49c2cfe4b6934fc23e5d8767fe719140c62c074a
Novkirishki <novkirishki94@gmail.com>
2016-02-11 18:03:23 +0200

    Added sceleton reviews page

---
+++ Web/BoardGames/Areas/Public/Controllers/ReviewsController.cs
@@ -0,0 +1,13 @@
+﻿namespace BoardGames.Areas.Public.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ReviewsController : Controller
+    {
+        // GET: Public/Reviews
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
