c4fb8de495aa376b8ca0394b5dec7c24352efec7
Novkirishki <novkirishki94@gmail.com>
2016-02-18 15:54:18 +0200

    Added image for date

--- Web/BoardGames/Areas/Private/Controllers/TutorialsController.cs
+++ Web/BoardGames/Areas/Private/Controllers/TutorialsController.cs
@@ -2,7 +2,10 @@
 {
     using Models;
     using Services.Data.Contracts;
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
+    using System.Web.Caching;
     using System.Web.Mvc;
     using Web.Infrastructure.Mapping;
 
@@ -20,30 +23,28 @@
         {
             var model = new TutorialsIndexViewModel();
 
-            //if (this.HttpContext.Cache.Get("Categories") != null)
-            //{
-            //    model.Categories = (List<CategoryViewModel>)this.HttpContext.Cache.Get("Categories");
-            //}
-            //else
-            //{
-            //    model.Categories = this.categories.GetAll().To<CategoryViewModel>().ToList();
-            //    this.HttpContext.Cache.Add("Categories", model.Categories, null, Cache.NoAbsoluteExpiration, new TimeSpan(0, 10, 0), CacheItemPriority.Normal, null);
-            //}
+            if (this.HttpContext.Cache.Get("BestTutorials") != null)
+            {
+                model.BestTutorials = (List<TutorialBestMenuViewModel>)this.HttpContext.Cache.Get("BestTutorials");
+            }
+            else
+            {
+                model.BestTutorials = this.tutorials.GetBest().To<TutorialBestMenuViewModel>().ToList();
+                this.HttpContext.Cache.Add("BestTutorials", model.BestTutorials, null, Cache.NoAbsoluteExpiration, new TimeSpan(0, 10, 0), CacheItemPriority.Normal, null);
+            }
 
-            //if (this.HttpContext.Cache.Get("LatestReviews") != null)
-            //{
-            //    model.LatestReviews = (List<ReviewMenuItemViewModel>)this.HttpContext.Cache.Get("LatestReviews");
-            //}
-            //else
-            //{
-            //    model.LatestReviews = this.reviews.GetLatest(3).To<ReviewMenuItemViewModel>().ToList();
-            //    this.HttpContext.Cache.Add("LatestReviews", model.LatestReviews, null, Cache.NoAbsoluteExpiration, new TimeSpan(0, 10, 0), CacheItemPriority.Normal, null);
-            //}
+            if (this.HttpContext.Cache.Get("RandomTutorials") != null)
+            {
+                model.RandomTutorials = (List<TutorialRandomMenuViewModel>)this.HttpContext.Cache.Get("RandomTutorials");
+            }
+            else
+            {
+                model.RandomTutorials = this.tutorials.GetRandom().To<TutorialRandomMenuViewModel>().ToList();
+                this.HttpContext.Cache.Add("RandomTutorials", model.RandomTutorials, null, Cache.NoAbsoluteExpiration, new TimeSpan(0, 3, 0), CacheItemPriority.Normal, null);
+            }
 
             model.PagesCount = this.tutorials.GetPagesCount();
             model.Tutorials = this.tutorials.GetByPage(page).To<TutorialListedViewModel>().ToList();
-            model.RandomTutorials = this.tutorials.GetRandom().To<TutorialRandomMenuViewModel>().ToList();
-            model.BestTutorials = this.tutorials.GetBest().To<TutorialBestMenuViewModel>().ToList();
             return View(model);
         }
     }
