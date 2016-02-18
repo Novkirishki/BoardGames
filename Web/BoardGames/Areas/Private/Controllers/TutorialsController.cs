2427b5034c7d105d95a04f60ae501bb0e0a87bfc
Novkirishki <novkirishki94@gmail.com>
2016-02-18 17:09:14 +0200

    Added tutorial details page

--- Web/BoardGames/Areas/Private/Controllers/TutorialsController.cs
+++ Web/BoardGames/Areas/Private/Controllers/TutorialsController.cs
@@ -1,10 +1,12 @@
 ﻿namespace BoardGames.Areas.Private.Controllers
 {
+    using Data.Models;
     using Models;
     using Services.Data.Contracts;
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Net;
     using System.Web.Caching;
     using System.Web.Mvc;
     using Web.Infrastructure.Mapping;
@@ -18,7 +20,6 @@
             this.tutorials = tutorials;
         }
 
-        // GET: Public/Reviews
         public ActionResult Index(int page = 1)
         {
             var model = new TutorialsIndexViewModel();
@@ -45,6 +46,45 @@
 
             model.PagesCount = this.tutorials.GetPagesCount();
             model.Tutorials = this.tutorials.GetByPage(page).To<TutorialListedViewModel>().ToList();
+            return View(model);
+        }
+        
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Tutorial tutorial = this.tutorials.GetById((int)id);
+            if (tutorial == null)
+            {
+                return HttpNotFound();
+            }
+
+            var model = new TutorialDetailsPageViewModel();
+
+            if (this.HttpContext.Cache.Get("BestTutorials") != null)
+            {
+                model.BestTutorials = (List<TutorialBestMenuViewModel>)this.HttpContext.Cache.Get("BestTutorials");
+            }
+            else
+            {
+                model.BestTutorials = this.tutorials.GetBest().To<TutorialBestMenuViewModel>().ToList();
+                this.HttpContext.Cache.Add("BestTutorials", model.BestTutorials, null, Cache.NoAbsoluteExpiration, new TimeSpan(0, 10, 0), CacheItemPriority.Normal, null);
+            }
+
+            if (this.HttpContext.Cache.Get("RandomTutorials") != null)
+            {
+                model.RandomTutorials = (List<TutorialRandomMenuViewModel>)this.HttpContext.Cache.Get("RandomTutorials");
+            }
+            else
+            {
+                model.RandomTutorials = this.tutorials.GetRandom().To<TutorialRandomMenuViewModel>().ToList();
+                this.HttpContext.Cache.Add("RandomTutorials", model.RandomTutorials, null, Cache.NoAbsoluteExpiration, new TimeSpan(0, 3, 0), CacheItemPriority.Normal, null);
+            }
+
+            model.Tutorial = AutoMapperConfig.Configuration.CreateMapper().Map<TutorialListedViewModel>(tutorial);
             return View(model);
         }
     }
---
+++ Web/BoardGames/Areas/Private/Models/TutorialDetailsPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BoardGames.Areas.Private.Models
+{
+    public class TutorialDetailsPageViewModel
+    {
+
+        public IEnumerable<TutorialBestMenuViewModel> BestTutorials { get; set; }
+
+        public IEnumerable<TutorialRandomMenuViewModel> RandomTutorials { get; set; }
+
+        public TutorialListedViewModel Tutorial { get; set; }
+    }
+}
