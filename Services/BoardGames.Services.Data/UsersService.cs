3f412fd66c5e6452cfccd9f0e5a96e13d89c3482
Novkirishki <novkirishki94@gmail.com>
2016-02-22 15:03:50 +0200

    Added users panel and service
    Users can be created, changed passwords, made admins

---
+++ Services/BoardGames.Services.Data/UsersService.cs
@@ -0,0 +1,74 @@
+﻿namespace BoardGames.Services.Data
+{
+    using BoardGames.Services.Data.Contracts;
+    using System.Linq;
+    using BoardGames.Data.Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity;
+    using System.Data.Entity;
+    using Common;
+    using Microsoft.AspNet.Identity.Owin;
+    using Microsoft.Owin.Security.DataProtection;
+
+    public class UsersService : IUsersService
+    {
+        private readonly DbContext context;
+        private readonly UserManager<User> userManager;
+
+        public UsersService(DbContext context)
+        {
+            this.context = context;
+            var userStore = new UserStore<User>(context);
+            userManager = new UserManager<User>(userStore);
+
+            var provider = new DpapiDataProtectionProvider("BoardGames");
+
+            userManager.UserTokenProvider = new DataProtectorTokenProvider<User>(
+                provider.Create("UserToken"));
+        }
+
+        public void Create(string email, string username, string password, bool isAdmin)
+        {
+            var user = new User { UserName = username, Email = email };
+            userManager.Create(user, password);
+
+            if (isAdmin)
+            {
+                userManager.AddToRole(user.Id, GlobalConstants.AdministratorRoleName);
+            }
+        }
+
+        public void Delete(string id)
+        {
+            var userToBeDeleted = userManager.FindById(id);
+            userManager.Delete(userToBeDeleted);
+        }
+
+        public void Edit(string id, string email, string username, string password, bool isAdmin)
+        {
+            var userToBeEdited = userManager.FindById(id);
+
+            userToBeEdited.Email = email;
+            userToBeEdited.UserName = username;
+
+            var token = userManager.GeneratePasswordResetToken(id);
+            userManager.ResetPassword(id, token, password);
+
+            if (isAdmin && userToBeEdited.Roles.Count == 0)
+            {
+                userManager.AddToRole(userToBeEdited.Id, GlobalConstants.AdministratorRoleName);
+            }
+            else if (!isAdmin && userToBeEdited.Roles.Count >= 1)
+            {
+                userManager.RemoveFromRole(userToBeEdited.Id, GlobalConstants.AdministratorRoleName);
+            }
+
+            userManager.Update(userToBeEdited);
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            return userManager.Users;
+        }
+    }
+}
---
+++ Web/BoardGames/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,75 @@
+﻿namespace BoardGames.Areas.Admin.Controllers
+{
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Web.Mvc;
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.UI;
+    using BoardGames.Data.Models;
+    using Services.Data.Contracts;
+    using Models;
+    using Web.Infrastructure.Mapping;
+    public class UsersController : Controller
+    {
+        private readonly IUsersService users;
+
+        public UsersController(IUsersService users)
+        {
+            this.users = users;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Users_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            DataSourceResult result = this.users
+                .GetAll()
+                .To<UserViewModel>()
+                .ToDataSourceResult(request);
+
+            return Json(result);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Users_Create([DataSourceRequest]DataSourceRequest request, UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                this.users.Create(
+                    model.Email,
+                    model.UserName,
+                    model.Password,
+                    model.IsAdmin);
+            }
+
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Users_Update([DataSourceRequest]DataSourceRequest request, UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                this.users.Edit(
+                    model.Id,
+                    model.Email,
+                    model.UserName,
+                    model.Password,
+                    model.IsAdmin);
+            }
+
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Users_Destroy([DataSourceRequest]DataSourceRequest request, UserViewModel model)
+        {
+            this.users.Delete(model.Id);
+
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState));
+        }
+    }
+}
