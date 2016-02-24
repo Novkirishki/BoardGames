5d4195ee64d08a744d3257cf5b62920e41c6e888
Novkirishki <novkirishki94@gmail.com>
2016-02-24 12:55:41 +0200

    Added controller tests

---
+++ Tests/BoardGames.Web.Controllers.Tests/PrivateCommentsControllerTests.cs
@@ -0,0 +1,88 @@
+﻿namespace BoardGames.Web.Controllers.Tests
+{
+    using Areas.Private.Controllers;
+    using Areas.Private.Models;
+    using Data.Models;
+    using Infrastructure.Mapping;
+    using Moq;
+    using NUnit.Framework;
+    using Services.Data.Contracts;
+    using System.Collections.Generic;
+    using System.Linq;
+    using TestStack.FluentMVCTesting;
+
+    [TestFixture]
+    public class PrivateCommentsControllerTests
+    {
+        private CommentsController commentsController;
+        private const int tutorialId = 2;
+        private const int pages = 7;
+        private const int page = 3;
+        private const string content = "content";
+
+        [TestFixtureSetUp]
+        public void PlanetAreaControllerTestsSetup()
+        {
+            var automapperConfig = new AutoMapperConfig();
+            automapperConfig.Execute(typeof(CommentsController).Assembly);
+
+            var commentsServiceMock = new Mock<ICommentsService>();
+            commentsServiceMock
+                .Setup(x => x.GetPagesCount(tutorialId))
+                .Returns(pages);
+
+            commentsServiceMock
+                .Setup(x => x.GetByPage(tutorialId, page))
+                .Returns(new List<Comment>()
+                {
+                    new Comment
+                    {
+                        Content = content,
+                        Tutorial = new Tutorial { Title = "" },
+                        Author = new User()
+                    },
+                    new Comment
+                    {
+                        Content = content,
+                        Tutorial = new Tutorial { Title = "" },                      
+                        Author = new User()
+                    }
+                }.AsQueryable());
+
+            commentsController = new CommentsController(commentsServiceMock.Object);
+        }
+
+        [Test]
+        public void TestGetReviewByIdWithValidData()
+        {
+            commentsController
+                .WithCallTo(x => x.GetByPage(2, 3))
+                .ShouldRenderPartialView("_CommentsPartial")
+                .WithModel<CommentsViewModel>(
+                    viewModel =>
+                    {
+                        Assert.AreEqual(pages, viewModel.Pages);
+                        Assert.AreEqual(content, viewModel.Comments.First().Content);
+                        Assert.AreEqual(2, viewModel.Comments.Count());
+                    }
+                )
+                .AndNoModelErrors();
+        }
+
+        [Test]
+        public void TestGetReviewByIdWithInvalidData()
+        {
+            commentsController
+                .WithCallTo(x => x.GetByPage(1, 12))
+                .ShouldRenderPartialView("_CommentsPartial")
+                .WithModel<CommentsViewModel>(
+                    viewModel =>
+                    {
+                        Assert.AreEqual(0, viewModel.Pages);
+                        Assert.AreEqual(0, viewModel.Comments.Count());
+                    }
+                )
+                .AndNoModelErrors();
+        }
+    }
+}
---
+++ Tests/BoardGames.Web.Controllers.Tests/PublicReviewsControllerTests.cs
@@ -0,0 +1,93 @@
+﻿namespace BoardGames.Web.Controllers.Tests
+{
+    using Areas.Admin.Models;
+    using Areas.Public.Controllers;
+    using Data.Models;
+    using Infrastructure.Mapping;
+    using Moq;
+    using NUnit.Framework;
+    using Services.Data.Contracts;
+    using System.Collections.Generic;
+    using System.Linq;
+    using TestStack.FluentMVCTesting;
+
+    [TestFixture]
+    public class PublicReviewsControllerTests
+    {
+        private ReviewsController reviewsController;
+        private const int reviewId = 13;
+        private const int page = 2;
+        private const string category = "test";
+        private const string reviewContent = "testContent";
+        private const string gameTitle = "title";
+
+        [TestFixtureSetUp]
+        public void PlanetAreaControllerTestsSetup()
+        {
+            var automapperConfig = new AutoMapperConfig();
+            automapperConfig.Execute(typeof(ReviewsController).Assembly);
+
+            var reviewsServiceMock = new Mock<IReviewsService>();
+            reviewsServiceMock
+                .Setup(x => x.GetById(reviewId))
+                .Returns(new Review()
+                {
+                    Content = reviewContent,
+                    Category = new Category() { Name = "Some" }
+                });
+
+            reviewsServiceMock
+                .Setup(x => x.GetByPageAndCategory(category, page))
+                .Returns(new List<Review>()
+                {
+                    new Review
+                    {
+                        Content = reviewContent,
+                        Category = new Category() { Name = "Some" },
+                        GameTitle = gameTitle
+                    },
+                    new Review
+                    {
+                        Content = reviewContent,
+                        Category = new Category() { Name = "Some" },
+                        GameTitle = gameTitle
+                    }
+                }.AsQueryable());
+
+            var categoriesServiceMock = new Mock<ICategoriesService>();
+            
+            reviewsController = new ReviewsController(reviewsServiceMock.Object, categoriesServiceMock.Object);
+        }
+
+        [Test]
+        public void TestGetReviewByIdWithValidId()
+        {
+            reviewsController
+                .WithCallTo(x => x.Details(reviewId))
+                .ShouldRenderView("Details")
+                .WithModel<ReviewViewModel>(
+                    viewModel =>
+                    {
+                        Assert.AreEqual(reviewContent, viewModel.Content);
+                    }
+                )
+                .AndNoModelErrors();
+        }
+
+        [Test]
+        public void TestGetReviewByIdWithNullIdShouldReturnBadRequest()
+        {
+            reviewsController
+                .WithCallTo(x => x.Details(null))
+                .ShouldGiveHttpStatus(400);
+        }
+
+        [Test]
+        public void TestGetReviewByIdWithInvalidIdShouldReturnNotFound()
+        {
+            reviewsController
+                .WithCallTo(x => x.Details(12))
+                .ShouldGiveHttpStatus(404);
+        }
+    }
+}
